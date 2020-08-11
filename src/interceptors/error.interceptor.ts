34a5831e56b047bff16bccdf89572433f272bf48
Romão moura <c.romaomoura@gmail.com>
2020-08-11 15:30:20 -0300

    Interceptor para incluir token nas requisições.

--- src/interceptors/error.interceptor.ts
+++ src/interceptors/error.interceptor.ts
@@ -6,7 +6,6 @@ import { Observable } from "rxjs/Rx";
 export class ErrorInterceptor implements HttpInterceptor {
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>>{
-    console.log("Passou aqui!")
     return next.handle(req)
     .catch((error, caught) => {
       let errorObj = error;
--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -1,5 +1,5 @@
 import { API_CONFIG } from "./../../config/api.config";
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs/Rx";
 import { ClienteDTO } from "../../models/cliente.dto";
@@ -9,12 +9,7 @@ import { StorageService } from "../storage.service";
 export class ClienteService {
   constructor(public http: HttpClient, public storage: StorageService) {}
   findByEmail(email: string): Observable<ClienteDTO> {
-    let token = this.storage.getLocalUser().token;
-    let authHeader = new HttpHeaders({'Authorization' : "Bearer " + token });
-    return this.http.get<ClienteDTO>(
-      `${API_CONFIG.baseURL}/clientes/email?value=${email}`,
-      { headers: authHeader }
-    );
+    return this.http.get<ClienteDTO>(`${API_CONFIG.baseURL}/clientes/email?value=${email}`);
   }
   getImageFromBucket(id : string) : Observable<any> {
     let url = `${API_CONFIG.bucketUrl}/cp${id}.jpg`
