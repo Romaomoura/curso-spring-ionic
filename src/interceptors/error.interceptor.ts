2221c63685efe0a2ea72ae44a246f4242bde8241
Romão moura <c.romaomoura@gmail.com>
2020-08-11 15:42:06 -0300

    Tratando erro 403.

--- src/interceptors/error.interceptor.ts
+++ src/interceptors/error.interceptor.ts
@@ -1,24 +1,44 @@
 import { Injectable } from "@angular/core";
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HTTP_INTERCEPTORS } from "@angular/common/http";
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+  HTTP_INTERCEPTORS,
+} from "@angular/common/http";
 import { Observable } from "rxjs/Rx";
+import { StorageService } from "../services/storage.service";
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
+  constructor(public storage: StorageService) {}
 
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>>{
-    return next.handle(req)
-    .catch((error, caught) => {
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    return next.handle(req).catch((error, caught) => {
       let errorObj = error;
-      if(errorObj.error){
+      if (errorObj.error) {
         errorObj = errorObj.error;
       }
-      if(!errorObj.status){
+      if (!errorObj.status) {
         errorObj = JSON.parse(errorObj);
       }
       console.log("Error detectado pelo interceptor");
       console.log(errorObj);
+
+      switch (errorObj.status) {
+        case 403:
+          this.handle403();
+          break;
+      }
+
       return Observable.throw(errorObj);
     }) as any;
+  }
+  handle403() {
+    this.storage.setLocalUser(null);
   }
 }
 
