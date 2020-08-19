b1ec2c5b5cb88425a1ec86e08afd8f913e5d5145
Romão moura <c.romaomoura@gmail.com>
2020-08-19 12:04:20 -0300

    Implementado componente loading em produtos.ts.

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -3,6 +3,8 @@ import { Component } from "@angular/core";
 import { IonicPage, NavController, NavParams } from "ionic-angular";
 import { ProdutoDTO } from "../../models/produto.dto";
 import { ProdutoService } from "../../services/domain/produto.service";
+import { LoadingController } from "ionic-angular/components/loading/loading-controller";
+
 
 @IonicPage()
 @Component({
@@ -15,17 +17,22 @@ export class ProdutosPage {
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
-    public produtoService: ProdutoService
+    public produtoService: ProdutoService,
+    public loadingCtrl: LoadingController
   ) {}
 
   ionViewDidLoad() {
     let categoria_id = this.navParams.get("categoria_id");
+    let loader = this.presentLoading();
     this.produtoService.findByCategoria(categoria_id).subscribe(
       (response) => {
         this.items = response["content"];
+        loader.dismiss();
         this.loadImageUrls();
       },
-      (error) => {}
+      (error) => {
+        loader.dismiss();
+      }
     );
   }
   loadImageUrls() {
@@ -41,5 +48,12 @@ export class ProdutosPage {
   }
   showDetail(produto_id: string){
     this.navCtrl.push('ProdutoDetailPage', {produto_id: produto_id});
+  }
+  presentLoading(){
+    let loader = this.loadingCtrl.create({
+      content: "Carregando..."
+    });
+    loader.present();
+    return loader;
   }
 }
