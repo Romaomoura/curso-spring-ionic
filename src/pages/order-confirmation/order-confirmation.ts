b79ca8ef97ec84f3cd43c1ac0df3d7a86943bf9b
Romão moura <c.romaomoura@gmail.com>
2020-08-19 10:25:48 -0300

    Salvando pedido

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -1,19 +1,19 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { PedidoDTO } from '../../models/pedido.dto';
-import { CartItem } from '../../models/cart-item';
-import { CartService } from '../../services/domain/cart.service';
-import { ClienteDTO } from '../../models/cliente.dto';
-import { EnderecoDTO } from '../../models/endereco.dto';
-import { ClienteService } from '../../services/domain/cliente.service';
+import { Component } from "@angular/core";
+import { IonicPage, NavController, NavParams } from "ionic-angular";
+import { PedidoDTO } from "../../models/pedido.dto";
+import { CartItem } from "../../models/cart-item";
+import { CartService } from "../../services/domain/cart.service";
+import { ClienteDTO } from "../../models/cliente.dto";
+import { EnderecoDTO } from "../../models/endereco.dto";
+import { ClienteService } from "../../services/domain/cliente.service";
+import { PedidoService } from "../../services/domain/pedido.service";
 
 @IonicPage()
 @Component({
-  selector: 'page-order-confirmation',
-  templateUrl: 'order-confirmation.html',
+  selector: "page-order-confirmation",
+  templateUrl: "order-confirmation.html",
 })
 export class OrderConfirmationPage {
-
   pedido: PedidoDTO;
   cartItems: CartItem[];
   cliente: ClienteDTO;
@@ -23,27 +23,31 @@ export class OrderConfirmationPage {
     public navCtrl: NavController,
     public navParams: NavParams,
     public cartService: CartService,
-    public clienteService: ClienteService) {
-
-      this.pedido = this.navParams.get('pedido');
+    public clienteService: ClienteService,
+    public pedidoService: PedidoService
+  ) {
+    this.pedido = this.navParams.get("pedido");
   }
 
   ionViewDidLoad() {
+    this.cartItems = this.cartService.getCart().items;
 
-   this.cartItems = this.cartService.getCart().items;
-
-   this.clienteService.findById(this.pedido.cliente.id)
-   .subscribe(response => {
-     this.cliente = response as ClienteDTO;
-     this.endereco = this.findByEndereco(this.pedido.enderecoEntrega.id, response['enderecos']);
-   },
-   error => {
-     this.navCtrl.setRoot('HomePage');
-   });
+    this.clienteService.findById(this.pedido.cliente.id).subscribe(
+      (response) => {
+        this.cliente = response as ClienteDTO;
+        this.endereco = this.findByEndereco(
+          this.pedido.enderecoEntrega.id,
+          response["enderecos"]
+        );
+      },
+      (error) => {
+        this.navCtrl.setRoot("HomePage");
+      }
+    );
   }
 
-  private findByEndereco(id: string, list: EnderecoDTO[]) : EnderecoDTO {
-    let position = list.findIndex(x => x.id == id);
+  private findByEndereco(id: string, list: EnderecoDTO[]): EnderecoDTO {
+    let position = list.findIndex((x) => x.id == id);
     return list[position];
   }
 
@@ -51,4 +55,22 @@ export class OrderConfirmationPage {
     return this.cartService.total();
   }
 
+  checkout() {
+    this.pedidoService.insert(this.pedido)
+    .subscribe(response => {
+        console.log(this.pedido);
+        this.cartService.createOrClearCart();
+        console.log(response.headers.get('location'));
+
+      },
+      error => {
+        if (error == 403) {
+          this.navCtrl.setRoot("HomePage");
+        }
+      }
+    );
+  }
+  back(){
+    this.navCtrl.setRoot('CartPage');
+  }
 }
---
+++ src/services/domain/pedido.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { API_CONFIG } from "../../config/api.config";
+import { PedidoDTO } from "../../models/pedido.dto";
+
+@Injectable()
+export class PedidoService {
+  constructor(public http : HttpClient) {}
+
+  insert(obj: PedidoDTO) {
+    return this.http.post(`${API_CONFIG.baseURL}/pedidos`, obj,
+      {
+        observe: "response",
+        responseType: "text"
+      }
+    );
+  }
+}
