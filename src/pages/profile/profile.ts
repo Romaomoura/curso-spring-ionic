f3f8f152ed67c23436f9aaef15fca71e735a5c52
Romão moura <c.romaomoura@gmail.com>
2020-08-21 11:32:10 -0300

    Criando metodos para tirar fotos pela camera do dispositivo ou pegar da galeria.

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -4,6 +4,7 @@ import { IonicPage, NavController, NavParams } from "ionic-angular";
 import { StorageService } from "../../services/storage.service";
 import { ClienteDTO } from "../../models/cliente.dto";
 import { ClienteService } from "../../services/domain/cliente.service";
+import { CameraOptions, Camera } from '@ionic-native/camera';
 
 @IonicPage()
 @Component({
@@ -12,16 +13,23 @@ import { ClienteService } from "../../services/domain/cliente.service";
 })
 export class ProfilePage {
   cliente: ClienteDTO;
+  picture: string;
+  cameraOn: boolean = false;
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     public storage: StorageService,
-    public clienteService: ClienteService
+    public clienteService: ClienteService,
+    private camera: Camera
   ) {}
 
   ionViewDidLoad() {
-    let localUser = this.storage.getLocalUser();
+   this.loadData();
+  }
+
+  loadData(){
+     let localUser = this.storage.getLocalUser();
     if (localUser && localUser.email) {
       this.clienteService.findByEmail(localUser.email).subscribe(
         (response) => {
@@ -47,4 +55,67 @@ export class ProfilePage {
       (error) => {}
     );
   }
+  // https://gist.github.com/frumbert/3bf7a68ffa2ba59061bdcfc016add9ee
+  blobToDataURL(blob) {
+    return new Promise((fulfill, reject) => {
+        let reader = new FileReader();
+        reader.onerror = reject;
+        reader.onload = (e) => fulfill(reader.result);
+        reader.readAsDataURL(blob);
+    })
+  }
+  getCameraPicture() {
+
+    this.cameraOn = true;
+
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.PNG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+     this.picture = 'data:image/png;base64,' + imageData;
+     this.cameraOn = false;
+    }, (err) => {
+      this.cameraOn = false;
+    });
+  }
+
+  sendPicture() {
+    this.clienteService.uploadPicture(this.picture)
+      .subscribe(response => {
+        this.picture = null;
+        this.loadData();
+        //this.getImageIfExists();
+      },
+      error => {
+      });
+  }
+
+  cancel() {
+    this.picture = null;
+  }
+  getGalleryPicture() {
+
+    this.cameraOn = true;
+
+    const options: CameraOptions = {
+      quality: 100,
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.PNG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+     this.picture = 'data:image/png;base64,' + imageData;
+     this.cameraOn = false;
+    }, (err) => {
+      this.cameraOn = false;
+    });
+  }
+
+
 }
